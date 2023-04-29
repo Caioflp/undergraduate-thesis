d908d2466a92c1771657493393a5e587dd8465fc
Caioflp <caiolinspeixoto@gmail.com>
2023-04-29 18:23:00 -0300

    Finishes first (untested) implementation of gradient descent for IV.

--- src/models.py
+++ src/models.py
@@ -2,31 +2,66 @@
 
 Author: @Caioflp
 """
+from dataclasses import dataclass
 from typing import Literal
 
 import numpy as np
+from sklearn.neighbours import KNeighborsRegressor
+from sklearn.base import BaseEstimator
 
 
-class SGDIVProjectedLoss:
-    """Implements a functional stochastic gradient descent algorithm for regression
+DEFAULT_REGRESSOR = KNeighborsRegressor(weights="distance", n_neighbors=5)
+
+
+@dataclass
+class Estimates:
+    """Holds sequence of estimates evaluated on grid points and on observed
+    points.
+
+    """
+    on_grid_points: np.ndarray
+    on_observed_poins: np.ndarray
+
+
+class FunctionalGradientDescentIV(BaseEstimator):
+    """Implements a functional gradient descent algorithm for regression
     on IV problems.
 
+    Parameters
+    ----------
+    lr : str, default "inv_n_samples"
+        A string which determines which learning rate to use. `"inv_sqrt"`
+        means `alpha_i = 1/sqrt(i)`. `"inv_n_samples"` means that the learning
+        rate is constant and equal to the inverse square root of the number of
+        training samples.
+
+    Attributes
+    ----------
 
     Notes
     -----
     In this formulation, the loss is computed after projecting onto Z, i.e.,
     .. math::
-        R(h) = \mathbf{E} [\ell (r_0(Z), h(Z))],
-    where :math:`r_0(Z) = \mathbf{E} [Y | Z]`.
+        R(h) = E [l (r_0(Z), Th(Z))],
+    where :math:`r_0(Z) = E [Y | Z]` and 
+    .. math::
+        Th(Z) = E [h(X) | Z].
 
     """
     def __init__(
         self,
         lr: Literal["inv_sqrt", "inv_n_samples"] = "inv_n_samples",
+        projector_y: BaseEstimator = DEFAULT_REGRESSOR,
+        projector_estimate: BaseEstimator = DEFAULT_REGRESSOR,
+        regressor_grad: BaseEstimator = DEFAULT_REGRESSOR,
     ):
-        pass
+        self.lr = lr
+        self.projector_y = projector_y,
+        self.projector_estimate = projector_estimate,
+        self.regressor_grad = regressor_grad,
 
-    def create_domain_grid(
+
+    def create_discretized_domain(
         self,
         X: np.ndarray,
         step: float = 1E-1
@@ -65,12 +100,11 @@ class SGDIVProjectedLoss:
         assert len(X.shape) == 2
         if len(X.shape) == 1:
             X = X[:, None]
-        # dimension
         dim = X.shape[1]
         lower_bound = X.min(axis=0)
         upper_bound = X.max(axis=0)
         amplitude = upper_bound - lower_bound
-        # Treat the 1 dimensional case separately, as it is cheaper
+        # Treat the 1 dimensional case separately, as it is cheaper.
         if dim == 1:
             return np.linspace(
                 lower_bound,
@@ -93,19 +127,85 @@ class SGDIVProjectedLoss:
         """Fits the estimator to iid data.
 
         """
+        n_samples = X.shape[0]
+        n_iter = n_samples
+
+        lr_dict = {
+            "inv_n_samples": lambda i: 1/np.sqrt(n_samples),
+            "inv_sqrt": lambda i: 1/np.sqrt(i)
+        }
+        lr = lr_dict[self.lr]
+
+        # Create domain for estimates.
+        x_domain = self.create_discretized_domain(X)
+        n_grid_points = x_domain.shape[0]
+
+        # Create object which will store the sequence of estimates evaluated on
+        # unobserved grid points and on observed random points.
+        estimates = Estimates(
+            on_grid_points=np.empty(n_iter+1, n_grid_points, dtype=np.float64),
+            on_observed_points=np.empty(n_iter+1, n_samples, dtype=np.float64)
+        )
+        estimates.on_grid_points[0, :] = np.zeros(n_grid_points)
+        estimates.on_observed_points[0, :] = np.zeros(n_samples)
+
+        # Compute the projected values of Y onto Z.
+        projected_y = projector_y.fit(Z, Y).predict(Z)
+
+        for i in range(1, n_iter+1):
+            # Project current estimate on Z, i.e., compute E [Th(X) | Z]
+            current_estimate = estimates.on_observed_points[i-1]
+            projected_current_estimate = \
+                    projector_estimate.fit(Z, current_estimate).predict(Z)
+
+            # Compute gradient of loss function.
+            # We are assuming that the loss function is quadratic, which makes
+            # its gradient linear.
+            # TODO: Implement other types of loss functions.
+            loss_grad = projected_current_estimate - projected_y
+
+            # Compute actual functional gradient, which is the regression of
+            # the loss gradient on X, i.e.,
+            # E [\partial_2 l (r_0(Z), Th(Z)) | X]
+            regressor_grad.fit(X, loss_grad)
+            functional_grad_grid = regressor_grad.predict(x_domain)
+            functional_grad_observed = regressor_grad.predict(X)
+
+            # Take one step in the negative gradient direction
+            estimates.on_grid_poins[i, :] =  (
+                estimates.on_grid_poins[i-1, :]
+                - lr(i) * functional_grad_grid
+            )
+            estimates.on_observed_poins[i, :] =  (
+                estimates.on_observed_poins[i-1, :]
+                - lr(i) * functional_grad_observed
+            )
+
+        # Construct final estimate as average of sequence of estimates
+        mean_estimate_on_grid = estimates.on_grid_points.mean(axis=0)
+        mean_estimate_on_observed = estimates.on_observed_points.mean(axis=0)
+        self.estimate_domain = np.concatenate((x_domain, X), axis=0)
+        self.estimate = np.concatenate(
+            mean_estimate_on_grid, mean_estimate_on_observed,
+            axis=None
+        )
+
+
 
     def predict(self, X: np.ndarray):
         """Applies the fitted function to given inputs.
 
         """
 
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    model = SGDIVProjectedLoss()
+
+    model = FunctionalGradientDescentIV()
 
     rng = np.random.default_rng()
     sample = rng.normal(size=(100, 2))
-    domain = model.create_domain_grid(sample, step=1E-1)
+    domain = model.create_discretized_domain(sample, step=1E-1)
     assert (domain.min(axis=0) == sample.min(axis=0)).all()
     assert (domain.max(axis=0) == sample.max(axis=0)).all()
 
