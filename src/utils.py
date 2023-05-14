b37d8909b26e3b03f1790f95047c7ffe0c62952f
Caioflp <caiolinspeixoto@gmail.com>
2023-05-14 16:01:51 -0300

    changes name -> path in experiment decorator

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,7 @@ from typing import Callable
 
 # Decorator with arguments syntax is weird
 # https://stackoverflow.com/questions/5929107/decorators-with-parameters
-def experiment(name: str = None):
+def experiment(path: str = None):
     """Marks a function as the main function of an experiment.
 
     Creates a separate directory for it to be run.
@@ -24,7 +24,7 @@ def experiment(name: str = None):
     if name is None:
         inner_output_dir = output_dir / now.strftime("%Y-%m-%d")
     else:
-        inner_output_dir = output_dir / name.lower().replace(" ", "_")
+        inner_output_dir = output_dir / path.lower().replace(" ", "_")
     run_dir = inner_output_dir / now.strftime("%H-%M-%S")
     run_dir.mkdir(parents=True, exist_ok=False)
 
