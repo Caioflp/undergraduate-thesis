d4ab3396c48b0fe1b070b8b977fd357501306289
Caioflp <caiolinspeixoto@gmail.com>
2023-05-14 11:37:43 -0300

    Adds functionality to choose output folder

--- src/utils.py
+++ src/utils.py
@@ -11,21 +11,27 @@ from functools import wraps
 from typing import Callable
 
 
-def experiment(func: Callable) -> Callable:
+# Decorator with arguments syntax is weird
+# https://stackoverflow.com/questions/5929107/decorators-with-parameters
+def experiment(name: str = None):
     """Marks a function as the main function of an experiment.
 
     Creates a separate directory for it to be run.
 
     """
-    now = datetime.now()
     output_dir = Path("./outputs")
-    day_dir = output_dir / now.strftime("%Y-%m-%d")
-    run_dir = day_dir / now.strftime("%H-%M-%S")
+    now = datetime.now()
+    if name is None:
+        inner_output_dir = output_dir / now.strftime("%Y-%m-%d")
+    else:
+        inner_output_dir = output_dir / name.lower().replace(" ", "_")
+    run_dir = inner_output_dir / now.strftime("%H-%M-%S")
     run_dir.mkdir(parents=True, exist_ok=False)
 
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        os.chdir(run_dir)
-        return func(*args, **kwargs)
-
-    return wrapper
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            os.chdir(run_dir)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
